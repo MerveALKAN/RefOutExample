ea1cc3c671cb1c3d15dbbd6c215771fad1651600
MerveALKAN <62889546+MerveALKAN@users.noreply.github.com>
2021-03-23 18:43:46 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace RefOutExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+
+            Degistir(ref x, ref y);
+
+            Console.WriteLine("X = " + x.ToString());
+            Console.WriteLine("Y = " + y.ToString());
+        }
+        static void Degistir(ref int x, ref int y) // metot çağırırken ref anahtar kelimesi kullanmak zorunludur 
+                                                   // kullanılacak değişkenlere mutlaka değer atanmalıdır.  
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+    }
+}
+// Derlendiğinde x ve y değişekenlerinin yerini değişir çünkü Degistir() metodundaki x ve y değişkenleri ile Main() x ve y değişkenleri
+// aynı bellek bölgesindeki değeri temsil etmektedir. Birinde yapılan değişiklik diğerini de etkilemektedir. 
+
+
+// 
+    
+
